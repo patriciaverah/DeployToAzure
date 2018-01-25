897f84977ec69615e9c6be1bfcf46e527023f521
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-01-25 10:27:10 +0200

    FirstExpress - my page added

--- routes/patricia.js
+++ routes/patricia.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('My deployment github repository can be found at https://github.com//DeployToAzure');
+  res.send('My deployment github repository can be found at https://github.com/patriciaverah/DeployToAzure');
 });
 
 module.exports = router;
