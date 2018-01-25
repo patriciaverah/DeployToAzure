c345257d376b99d5fcf17739fb211528ae91b099
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-01-25 11:16:18 +0200

    FirstExpress - patricia.pug

--- routes/patricia.js
+++ routes/patricia.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('My deployment github repository can be found at https://github.com/patriciaverah/DeployToAzure');
+  res.render('patricia', { link: 'https://github.com/patriciaverah/DeployToAzure'});
 });
 
 module.exports = router;
