2e468f27ee3a59c4f9efc99d37dd0533fb57cc02
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-12 19:32:24 +0200

    Football example

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var about = require('./routes/about');
-var patricia = require('./routes/patricia');
+var index = require('./app_server/routes/index');
+var football = require('./app_server/routes/football');
+var goldenball = require('./app_server/routes/goldenball');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
@@ -25,10 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-app.use('/about', about);
-app.use('/patricia', patricia);
-
+app.use('/football', football);
+app.use('/goldenball', goldenball);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
