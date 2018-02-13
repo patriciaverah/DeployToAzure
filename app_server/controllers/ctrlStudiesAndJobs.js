6e9d85b1e6980762a41012db2fb4476919a31f64
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 17:05:23 +0200

    Studies and jobs updated

--- app_server/controllers/ctrlStudiesAndJobs.js
+++ app_server/controllers/ctrlStudiesAndJobs.js
@@ -1,17 +1,18 @@
 /* GET home page */
 var winnerlist = function(req, res){
     res.render('studiesandjobs',{
-        winners:
+        studiesandjobs:
         [
-            {year:'1990', team:'Germany'},
-            {year:'1994', team:'Brazil'},
-            {year:'1998', team:'France'},
-            {year:'2002', team:'Brazil'},
-            {year:'2006', team:'Italy'},
-            {year:'2010', team:'Spain'},
-            {year:'2014', team:'Germany'}
+            {category: 'study', year:'unknown', what:'B2 - English'},
+            {category: 'study', year:'unknown', what:'A2 - German'}
+            {category: 'study', year:'unknown', what:'Photography courses online and in Universidad Pública de Navarra'}
+            {category: 'study', year:'2014-2018', what:'Computer Science, Universidad Pública de Navarra'},
+            {category: 'work', year:'2017', what:'Saleswoman in Stradivarius'},
+            {category: 'work', year:'2017', what:'Saleswoman in El Corte Inglés'},
+            {category: 'work', year:'2017', what:'Software developer in GeoActios'},
+            {category: 'study', year:'2018', what:'Exchange semester, Laurea University of Applied Sciences'}
         ]});
 };
 module.exports = {
-    winnerlist
+    studiesandjobsList
 };
