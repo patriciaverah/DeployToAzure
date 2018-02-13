e46c72bec1c61689537a68dc2eee6ed5bf5ae351
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 18:15:10 +0200

    links added - not working

--- app_server/routes/studiesandjobs.js
+++ app_server/routes/studiesandjobs.js
@@ -6,7 +6,7 @@ var ctrlStudiesAndJobs = require('../controllers/ctrlStudiesAndJobs');
 /* GET home page. */
 router.get('/', ctrlStudiesAndJobs.winnerlist);
 router.get('/', function(req, res, next) {
-  res.render('studiesandjobs', { link: 'https://www.stradivarius.com/'});
+  res.render('studiesandjobs', { link: 'https://www.stradivarius.com/', link2: 'https://www.elcorteingles.es/', link3: 'https://geoactio.com/#home'});
 });
 
 module.exports = router;
