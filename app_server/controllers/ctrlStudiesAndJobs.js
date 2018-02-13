6be57fcf69b49ebc6b787aee5e7cc541af808fdf
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 17:49:15 +0200

    addition and changes in order

--- app_server/controllers/ctrlStudiesAndJobs.js
+++ app_server/controllers/ctrlStudiesAndJobs.js
@@ -3,14 +3,15 @@ var winnerlist = function(req, res){
     res.render('studiesandjobs',{
         winners:
         [
-            {category: 'study', year:'unknown', what:'B2 - English'},
-            {category: 'study', year:'unknown', what:'A2 - German'},
-            {category: 'study', year:'unknown', what:'Photography courses online and in Universidad Pública de Navarra'},
             {category: 'study', year:'2014-2018', what:'Computer Science, Universidad Pública de Navarra'},
             {category: 'work', year:'2017', what:'Saleswoman in Stradivarius'},
             {category: 'work', year:'2017', what:'Saleswoman in El Corte Inglés'},
             {category: 'work', year:'2017', what:'Software developer in GeoActios'},
-            {category: 'study', year:'2018', what:'Exchange semester, Laurea University of Applied Sciences'}
+            {category: 'study', year:'2018', what:'Exchange semester, Laurea University of Applied Sciences'},
+
+            {category: 'study', year:'unknown', what:'B2 - English'},
+            {category: 'study', year:'unknown', what:'A2 - German'},
+            {category: 'study', year:'unknown', what:'Photography courses online and in Universidad Pública de Navarra'}
         ]});
 };
 module.exports = {
