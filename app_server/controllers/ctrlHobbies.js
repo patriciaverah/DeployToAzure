b9e9eed6449dcfda72f651d4e0931dc0190f2cfd
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 19:26:13 +0200

    hobbies + contests

--- app_server/controllers/ctrlHobbies.js
+++ app_server/controllers/ctrlHobbies.js
@@ -1,6 +1,11 @@
 /* GET home page */
 var winnerlist = function(req, res){
-    res.render('hobbies', {link:'https://www.flickr.com/photos/patriciaverah/'});
+    res.render('hobbies', {link:'https://www.flickr.com/photos/patriciaverah/',
+                          winners:
+                          [
+                              {year: '2017', contest: 'Google Hashcode'},
+                              {year: '2018', contest: 'Nokia Hackathon, Helsinki - got the 3rd place!'},
+                          ]});
 };
 module.exports = {
     winnerlist
