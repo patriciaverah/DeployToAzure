717e8aadcd1bcbbf4cc3660a0c87006746a7aa57
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 18:23:59 +0200

    hobbies updated

--- app_server/controllers/ctrlHobbies.js
+++ app_server/controllers/ctrlHobbies.js
@@ -3,13 +3,7 @@ var winnerlist = function(req, res){
     res.render('hobbies',{
         winners:
         [
-            {year:'1990', player:'Salvatore Schillaci', team: 'Italy'},
-            {year:'1994', player:'Romário', team: 'Brazil'},
-            {year:'1998', player:'Ronaldo', team: 'Brazil'},
-            {year:'2002', player:'Oliver Khan', team: 'Germany'},
-            {year:'2006', player:'Zinedine Zidane', team: 'France'},
-            {year:'2010', player:'Diego Forlán', team: 'Uruguay'},
-            {year:'2014', player:'Lionel Messi', team: 'Argentina'}
+            {socialmedia:'Flickr', link:'https://www.flickr.com/photos/patriciaverah/'}
         ]});
 };
 module.exports = {
