8eede294bcc3a796fb0ac566abe94205adf0b327
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 18:54:54 +0200

    link to github

--- app_server/controllers/ctrlMain.js
+++ app_server/controllers/ctrlMain.js
@@ -1,6 +1,6 @@
 /* GET home page */
 var index = function(req, res){
-    res.render('index');
+    res.render('index', {link: 'https://github.com/patriciaverah/DeployToAzure'});
 };
 
 module.exports = {
