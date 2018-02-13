8f2b6dd19e5000312c7c5b82ac1032af1919c86b
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 17:25:02 +0200

    names back

--- app_server/routes/studiesandjobs.js
+++ app_server/routes/studiesandjobs.js
@@ -4,6 +4,6 @@ var router = express.Router();
 var ctrlStudiesAndJobs = require('../controllers/ctrlStudiesAndJobs');
 
 /* GET home page. */
-router.get('/', ctrlStudiesAndJobs.studiesandjobslist);
+router.get('/', ctrlStudiesAndJobs.winnerlist);
 
 module.exports = router;
