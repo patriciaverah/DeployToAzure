28b4de1d42b2528bd9797e8b4e63dc115deb5cc7
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 16:38:14 +0200

    back

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./app_server/routes/index');
-var football = require('./app_server/routes/studiesandjobs');
-var goldenball = require('./app_server/routes/hobbies');
+var football = require('./app_server/routes/football');
+var goldenball = require('./app_server/routes/goldenball');
 
 var app = express();
 
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/studiesandjobs', studiesandjobs);
-app.use('/hobbies', hobbies);
+app.use('/football', football);
+app.use('/goldenball', goldenball);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
