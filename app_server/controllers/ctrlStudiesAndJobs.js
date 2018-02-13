d3d97012e56631a7c495de4594f03cdcdb1039f1
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 17:17:19 +0200

    comas

--- app_server/controllers/ctrlStudiesAndJobs.js
+++ app_server/controllers/ctrlStudiesAndJobs.js
@@ -4,8 +4,8 @@ var winnerlist = function(req, res){
         winners:
         [
             {category: 'study', year:'unknown', what:'B2 - English'},
-            {category: 'study', year:'unknown', what:'A2 - German'}
-            {category: 'study', year:'unknown', what:'Photography courses online and in Universidad Pública de Navarra'}
+            {category: 'study', year:'unknown', what:'A2 - German'},
+            {category: 'study', year:'unknown', what:'Photography courses online and in Universidad Pública de Navarra'},
             {category: 'study', year:'2014-2018', what:'Computer Science, Universidad Pública de Navarra'},
             {category: 'work', year:'2017', what:'Saleswoman in Stradivarius'},
             {category: 'work', year:'2017', what:'Saleswoman in El Corte Inglés'},
