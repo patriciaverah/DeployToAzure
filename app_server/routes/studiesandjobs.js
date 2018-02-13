fc3b9802a61959d5f1e9c72fbcb0e3252be56cbc
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 18:06:28 +0200

    links form changed

--- app_server/routes/studiesandjobs.js
+++ app_server/routes/studiesandjobs.js
@@ -5,5 +5,8 @@ var ctrlStudiesAndJobs = require('../controllers/ctrlStudiesAndJobs');
 
 /* GET home page. */
 router.get('/', ctrlStudiesAndJobs.winnerlist);
+router.get('/', function(req, res, next) {
+  res.render('studiesandjobs', { link: 'https://www.stradivarius.com/'});
+});
 
 module.exports = router;
