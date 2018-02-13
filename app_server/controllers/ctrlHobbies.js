0cfa98855e6f53ebfe0b025553bf1d15796b8ec1
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-13 18:42:25 +0200

    trial 2

--- app_server/controllers/ctrlHobbies.js
+++ app_server/controllers/ctrlHobbies.js
@@ -1,5 +1,7 @@
 /* GET home page */
 var winnerlist = function(req, res){
-    res.render('hobbies', {link: 'https://www.flickr.com/photos/patriciaverah/'});
+    res.render('hobbies', {link:'https://www.flickr.com/photos/patriciaverah/'});
 };
-module.exports = router;
+module.exports = {
+    winnerlist
+};
