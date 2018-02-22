0e2d0a15a23aef026c8450853cff9fe968138b68
patriciaverah <35450776+patriciaverah@users.noreply.github.com>
2018-02-22 14:29:33 +0200

    wrong name again

--- app_server/controllers/ctrlContact.js
+++ app_server/controllers/ctrlContact.js
@@ -1,7 +1,7 @@
 /* GET home page */
 var contact = function(req, res){
-    res.render('contact', {personal-email: 'p.vhernandez@hotmail.com',
-                           uni-email: 'patricia.vera.hernandez@student.laurea.com'});
+    res.render('contact', {personal: 'p.vhernandez@hotmail.com',
+                           uni: 'patricia.vera.hernandez@student.laurea.com'});
 };
 
 module.exports = {
