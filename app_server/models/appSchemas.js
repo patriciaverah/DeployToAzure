7de2b62a7528ca1562e5cd0683ad824632c91560
Patricia Vera H <patricia.vera.hernandez@student.laurea.fi>
2018-03-16 16:21:15 +0200

    fix ?

--- app_server/models/appSchemas.js
+++ app_server/models/appSchemas.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 // Schemas define the document structure used in the app
-const hobbiesSchema = new mongoose.Schema({year: String, contest: String, link: String});
+const hobbiesSchema = new mongoose.Schema({year: String, contest: String});
 const stuiesandjobsSchema = new mongoose.Schema({category: String, year: String, what: String, link: String});
-const contactSchema = new mongoose.Schema({link: String});
+const contactSchema = new mongoose.Schema({contact: String, description: String});
 
 /*  Models will contain compiled instances of the schemas
  *  so they can be used in the app later on
--- app_server/routes/contests.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var ctrlHobbies = require('../controllers/ctrlContests');
-
-/* GET home page. */
-router.get('/', ctrlContests.winnerlist);
-
-module.exports = router;
