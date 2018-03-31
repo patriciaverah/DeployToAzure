a02768481c87593b6fff4f9df63767d323273c88
Patricia Vera H <patricia.vera.hernandez@student.laurea.fi>
2018-03-31 13:26:44 +0300

    db

---
+++ app_server/models/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb://patricia:tn7p7qtp@ds261078.mlab.com:61078/patriciacv'
+
+mongoose.connect(dbURI);
+
+// Printing a connection status message to the console
+mongoose.connection.on('connected', function() {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ', err);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
