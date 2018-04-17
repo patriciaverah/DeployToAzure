0ac40cb6c56fe1a46de01c5c531475be9f3f45ed
Patricia Vera H <patricia.vera.hernandez@student.laurea.fi>
2018-04-17 18:50:18 +0300

    index trial 2

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,13 +3,19 @@ const router = express.Router();
 
 // Declaring the controllers
 const ctrlMain = require('../controllers/ctrlMain');
+const ctrlContact = require('../controllers/ctrlContact');
+const ctrlHobbies = require('../controllers/ctrlHobbies');
+const ctrlStudiesandjobs = require('../controllers/ctrlStudiesandjobs');
 
 // Defining the routes and associating the routes to their corresponding 
 router.get('/', ctrlMain.index); // Home page
+router.get('/hobbies', ctrlHobbies.hobbies); 
+router.get('/contact'. ctrlContact.contact);
+router.get('/studiesandjobs', ctrlStudiesandjobs.studiesandjobs);
 
 router
     .route('/studiesandjobs/add')
     .get(ctrlStudiesandjobs.showForm) // Display the form
-    .post(ctrlStudiesandjobs.addData) // Get from data and make the API call
+    .post(ctrlStudiesandjobs.addData); // Get from data and make the API call
 
 module.exports = router;
