9e9835182966a745a3e69ac5d3ed75485d4e953a
Patricia Vera H <patricia.vera.hernandez@student.laurea.fi>
2018-04-17 17:49:35 +0300

    back

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,19 +3,13 @@ const router = express.Router();
 
 // Declaring the controllers
 const ctrlMain = require('../controllers/ctrlMain');
-// const ctrlContact = require('../controllers/ctrlApiContact');
-// const ctrlHobbies = require('../controllers/ctrlApiHobbies');
-// const ctrlStudiesandjobs = require('../controllers/ctrlApiStudiesandjobs');
 
 // Defining the routes and associating the routes to their corresponding 
 router.get('/', ctrlMain.index); // Home page
-// router.get('/hobbies', ctrlHobbies.hobbies); 
-// router.get('/contact'. ctrlContact.contact);
-// router.get('/studiesandjobs', ctrlStudiesandjobs.studiesandjobs);
 
-router
+/*router
     .route('/studiesandjobs/add')
     .get(ctrlStudiesandjobs.showForm) // Display the form
     .post(ctrlStudiesandjobs.addData) // Get from data and make the API call
-
+*/
 module.exports = router;
