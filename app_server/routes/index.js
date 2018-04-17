c5255961bc86b458d4cc26e08613b0e91b873393
Patricia Vera H <patricia.vera.hernandez@student.laurea.fi>
2018-04-17 18:55:00 +0300

    index back 2

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 // Declaring the controllers
 const ctrlMain = require('../controllers/ctrlMain');
-const ctrlContact = require('../controllers/ctrlContact');
-const ctrlHobbies = require('../controllers/ctrlHobbies');
-const ctrlStudiesandjobs = require('../controllers/ctrlStudiesandjobs');
+// const ctrlContact = require('../controllers/ctrlContact');
+// const ctrlHobbies = require('../controllers/ctrlHobbies');
+// const ctrlStudiesandjobs = require('../controllers/ctrlStudiesandjobs');
 
 // Defining the routes and associating the routes to their corresponding 
 router.get('/', ctrlMain.index); // Home page
-router.get('/hobbies', ctrlHobbies.hobbies); 
-router.get('/contact'. ctrlContact.contact);
-router.get('/studiesandjobs', ctrlStudiesandjobs.studiesandjobs);
+// router.get('/hobbies', ctrlHobbies.hobbies); 
+// router.get('/contact'. ctrlContact.contact);
+// router.get('/studiesandjobs', ctrlStudiesandjobs.studiesandjobs);
 
 router
     .route('/studiesandjobs/add')
