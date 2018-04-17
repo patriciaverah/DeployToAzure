004c59f4bc745c2da40dce2828744932090f9b89
Patricia Vera H <patricia.vera.hernandez@student.laurea.fi>
2018-04-17 17:45:25 +0300

    erease

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 // Declaring the controllers
 const ctrlMain = require('../controllers/ctrlMain');
-const ctrlContact = require('../controllers/ctrlApiContact');
-const ctrlHobbies = require('../controllers/ctrlApiHobbies');
-const ctrlStudiesandjobs = require('../controllers/ctrlApiStudiesandjobs');
+// const ctrlContact = require('../controllers/ctrlApiContact');
+// const ctrlHobbies = require('../controllers/ctrlApiHobbies');
+// const ctrlStudiesandjobs = require('../controllers/ctrlApiStudiesandjobs');
 
 // Defining the routes and associating the routes to their corresponding 
 router.get('/', ctrlMain.index); // Home page
-router.get('/hobbies', ctrlHobbies.hobbies); 
-router.get('/contact'. ctrlContact.contact);
-router.get('/studiesandjobs', ctrlStudiesandjobs.studiesandjobs);
+// router.get('/hobbies', ctrlHobbies.hobbies); 
+// router.get('/contact'. ctrlContact.contact);
+// router.get('/studiesandjobs', ctrlStudiesandjobs.studiesandjobs);
 
 router
     .route('/studiesandjobs/add')
