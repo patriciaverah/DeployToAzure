a1475e5e14b3486288d9920432807f1b15533051
Patricia Vera H <patricia.vera.hernandez@student.laurea.fi>
2018-04-17 19:00:22 +0300

    beginning

--- app_server/controllers/ctrlStudiesAndJobs.js
+++ app_server/controllers/ctrlStudiesAndJobs.js
@@ -2,10 +2,6 @@
 const request = require('request');
 const apiURL = require('./apiURLs');
 
-/*const showForm = function(req, res) {
-    res.render('studiesandjobs_add');
-};*/
-
 const studiesandjobs = function(req, res) {
     const path = '/api/studiesandjobs';
     const requestOptions = {
@@ -32,34 +28,6 @@ const studiesandjobs = function(req, res) {
         }
     );
 };
-
-/*const addData = function(req, res){
-    const path = 'api/studiesandjobs';
-
-    const postdata = {
-        category: req.body.category,
-        year: req.body.year,
-        what: req.body.what,
-        link: req.body.link,
-    };
-
-    const requestOptions = {
-        url: apiURL.server + path,
-        method: 'POST',
-        json: postdata
-    };
-
-    request(
-        requestOptions,
-        function (err, response) {
-            if (response.statusCode === 201) {
-                res.redirect('/studiesandjobs');
-            } else {
-                res.render('error', {message: 'Error adding data: ' + response.statusMessage + ' (' + response.statusCode + ')'});
-            }
-        }
-    );
-};*/
 
 /*var winnerlist = function(req, res){
     res.render('studiesandjobs',{
